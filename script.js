2cf4af265498032a88a319eb3588ea67132da6da
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-04-30 23:52:01 +0530

    created the js file and there created the one page site there canot scrole and there need to use the nav bar

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("hi");
+function pressCustomer(){
+    //HERE SHOWING THE CUSTOMER SECTION 
+    document.getElementById("Customer").style.display="block";
+
+    //NOW NEED TO HIDE ANOTHER ALL
+    document.getElementById("hero").style.display="none";
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("Item").style.display="none";
+    document.getElementById("Orders").style.display="none";
+}
+
+function pressItem(){
+    //HERE SHOWING THE CUSTOMER SECTION 
+    document.getElementById("Item").style.display="block";
+
+    //NOW NEED TO HIDE ANOTHER ALL
+    document.getElementById("hero").style.display="none";
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("Customer").style.display="none";
+    document.getElementById("Orders").style.display="none";
+}
+
+function pressOrders(){
+    //HERE SHOWING THE CUSTOMER SECTION 
+    document.getElementById("Orders").style.display="block";
+
+    //NOW NEED TO HIDE ANOTHER ALL
+    document.getElementById("hero").style.display="none";
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("Customer").style.display="none";
+    document.getElementById("Item").style.display="none";
+}
