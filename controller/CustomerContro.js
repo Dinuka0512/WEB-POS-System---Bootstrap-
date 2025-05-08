5b03379a9295a366f75317a8ff4200738fa21a20
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-05-09 00:36:59 +0530

    created the genarating Id

--- controller/CustomerContro.js
+++ controller/CustomerContro.js
@@ -1,3 +1,23 @@
 import { customer_db } from '../db/db.js';
 
-console.log(customer_db);
+//TEXT FEILDS
+let custId = document.getElementById("custId");
+let txtFName = document.getElementById("custFName");
+let txtLName = document.getElementById("custLname");
+let txtEmail = document.getElementById("custEmail");
+let txtContact = document.getElementById("custContact");
+
+
+loadCustIds();
+
+
+// GENERATE  THE CUST ID
+function loadCustIds(){
+    custId.innerText = "C" + String(customer_db.length + 1).padStart(3,"0");
+}
+
+
+
+document.getElementById("CustSave").addEventListener('click', function(){
+    
+})
