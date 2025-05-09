ee0102d3f3ce73f425d2379a0f51f6d2d24d3a3e
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-05-09 07:48:07 +0530

    created the save method and page reset

---
+++ Util/Validation.js
@@ -0,0 +1,16 @@
+export class Validation {
+    
+    static isNameValid(name) {
+        return /^[A-Za-z]+$/.test(name);
+    }
+
+    static isEmailValid(email) {
+        return /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(email);
+    }
+
+    static isContactValid(contact) {
+        return /^\d{10}$/.test(contact);
+    }
+    
+    // Add more methods as needed
+}
--- controller/CustomerContro.js
+++ controller/CustomerContro.js
@@ -1,4 +1,5 @@
 import { customer_db } from '../db/db.js';
+import { Validation } from '../Util/Validation.js';
 
 //TEXT FEILDS
 let custId = document.getElementById("custId");
@@ -19,5 +20,58 @@ function loadCustIds(){
 
 
 document.getElementById("CustSave").addEventListener('click', function(){
-    
+    let id = custId.innerText;
+    let fname = txtFName.value;
+    let lName = txtLName.value;
+    let email = txtEmail.value;
+    let contact = txtContact.value;
+
+
+    // CREATEING CUSTOMER OBJ
+    let customer = {
+        custId : id,
+        custFname : fname,
+        cuatLname : lName,
+        custEmail : email,
+        custContact : contact
+    }
+
+    if(Validation.isNameValid(fname)){
+        if(Validation.isNameValid(lName)){
+            if(Validation.isEmailValid(email)){
+                if(Validation.isContactValid(contact)){
+                    //All OK
+                    save(customer);
+                }else{
+                    Swal.fire("Please Enter The valid Contact");
+                }
+            }else{
+                Swal.fire("Please enter the valid email");
+            }
+        }else{
+            Swal.fire("Please enter the valid name, The Name Only can have letters");
+        }
+    }else{
+        Swal.fire("Please enter the valid name, The Name Only can have letters");
+    }
 })
+
+
+// CUSTOMER SAVE HERE
+function save(custObj){
+    customer_db.push(custObj);
+    console.log(customer_db);
+
+    pageReset();
+}
+
+
+//HERE RESET THE PAGE
+function pageReset(){
+    txtFName.value = "";
+    txtLName.value = "";
+    txtEmail.value = "";
+    txtContact.value = "";
+    
+    loadCustIds();
+}
