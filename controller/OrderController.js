735ddf17e981ef23a015544ebe65b6ae011eb952
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-05-17 02:01:46 +0530

    Added the JQueree And Started the create Order Controller

---
+++ controller/OrderController.js
@@ -0,0 +1,25 @@
+import{order_db}from '../db/db.js';
+import { Validation } from '../Util/Validation.js';
+
+// TABLE 
+let tbody = $("#orderTBody");
+
+// BUTTONS
+let btnSave = $("#orderSave");
+let btnUpdate = $("#orderUpdate");
+let btnDelete = $("#orderDelete");
+let btnReset = $("#orderReset");
+
+
+pageReset();
+function pageReset(){
+    //SET DISPLAY NONE (CUSTOMERS & ITEM DETALS)
+    $('#custDetails').css('display','none');
+    $('#itemDetails').css('display','none');
+
+    //SET BUTTONS (UPDATE / DELETE / RESET) DISABLE
+    btnSave.prop("disabled", false);
+    btnDelete.prop("disabled", true);
+    btnReset.prop("disabled", true);
+    btnUpdate.prop("disabled", true);
+}
