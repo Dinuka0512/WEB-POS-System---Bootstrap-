3b550e2c963e5edb6dcbb1f037c57377ab7af592
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-05-17 10:21:39 +0530

    creating the order controller

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,4 +1,6 @@
 import{order_db}from '../db/db.js';
+import{customer_db}from '../db/db.js';
+import{item_db}from '../db/db.js';
 import { Validation } from '../Util/Validation.js';
 
 // TABLE 
@@ -10,6 +12,14 @@ let btnUpdate = $("#orderUpdate");
 let btnDelete = $("#orderDelete");
 let btnReset = $("#orderReset");
 
+//DROPDOWN MENU
+let comboItem = $("#ItemIdSelector");
+let comboCustomer = $("#CustomerSelector");
+
+
+btnReset.on('click', function(){
+    pageReset();
+})
 
 pageReset();
 function pageReset(){
@@ -23,3 +33,24 @@ function pageReset(){
     btnReset.prop("disabled", true);
     btnUpdate.prop("disabled", true);
 }
+
+
+function loadCustomerIdsToDropDown(){
+    for(let i = 0; i < customer_db.length; i++){
+        comboCustomer.append("<option>"+ customer_db[i].custId +"</option>");
+    }
+}
+
+function loadItemIdsToDropDown(){
+    for(let i = 0; i < item_db.length; i++){
+        comboItem.append("<option>"+ item_db[i].item_Id +"</option>");
+    }
+}
+
+
+
+$("#Orders").on("mouseenter", function(){
+    //LOAD IDS (CUSTOMERS / ITEMS)
+    loadCustomerIdsToDropDown();
+    loadItemIdsToDropDown();
+})
